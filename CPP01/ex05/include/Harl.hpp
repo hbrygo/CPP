7ced0cfb1d65c92f870b142fffa375c00a9c9473
Hugo Brygo <hubrygo@fu-c4-p2.s19.be>
2024-02-12 15:20:20 +0100

    correction ex05

--- CPP01/ex05/include/Harl.hpp
+++ CPP01/ex05/include/Harl.hpp
@@ -8,22 +8,10 @@ class Harl
 {
 private:
 	void (Harl::*_fct[4])( void );
-	void debug( void )
-	{
-		std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do !" << std::endl;
-	}
-	void info( void )
-	{
-		std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
-	}
-	void warning( void )
-	{
-		std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
-	}
-	void error( void )
-	{
-		std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
-	}
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
 
 public:
 	Harl();
